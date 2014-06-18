3fd59a4ed97ceac4b97864370eff63507706c5f9
Josh Fry <jdoe@example.com>
2014-06-18 00:47:45 -0700

    current date jd

--- src/js/init.js
+++ src/js/init.js
@@ -12,3 +12,15 @@ window.addEventListener('load', function() {
 
 // Init mobileMenu + options
 // $('.mobile-menu').mobileMenu();
+
+
+// print date
+
+var m_names = new Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+
+var d = new Date();
+var curr_date = d.getDate();
+var curr_month = d.getMonth();
+var curr_year = d.getFullYear();
+
+$('#print-current-date').text(m_names[curr_month] + "/" + curr_date + "/" + curr_year);
